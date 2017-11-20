128c4648838fd1f0279919c5eaa4bae91770f8f0
liang.chen <liang.chen@funplus.com>
2017-11-20 18:34:04 +0800

    add sender for call

--- sender.py
+++ sender.py
@@ -3,6 +3,8 @@ from message_xsend import MESSAGEXsend
 from app_configs import MESSAGE_CONFIGS
 from flask import Flask, request
 from flask.ext import restful
+import requests
+import json
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -12,7 +14,7 @@ api = restful.Api(app)
 
 class sendMessage(restful.Resource):
     def get(self):
-        return "xxxxxxxxxx"
+        return "message"
     def post(self):
         cellPhone = request.form['tos']
         content = request.form['content']
@@ -21,7 +23,37 @@ class sendMessage(restful.Resource):
         submail.set_project("RIEdd4")
         submail.add_var('txt', content)
         return submail.xsend()
-api.add_resource(sendMessage, '/api/v2/send')
+
+class sendCall(restful.Resource):
+    def get(self):
+        return "call"
+
+    def post(self):
+        _to = request.form['tos']
+        _message = request.form['content']
+        message_url = "http://sms-api.funplusgame.com/api/v2/send"
+
+        header = {
+            'Token': 'xxxxxx'
+        }
+    
+        body = {
+            "type": "call",
+            "tel": _to,
+            "csc": "86",
+            "message": "有报警," + str(_message) + "详情请查看邮件或者打开Duty页面; 有报警," + str(_message) + "详情请查看邮件或者打开Duty页面"
+        }
+    
+        response = requests.post(message_url, data=json.dumps(body), headers=header)
+        ret = json.loads(response.text)
+        if ret.get('status', 0):
+            return True
+        else:
+            return False
+
+
+api.add_resource(sendMessage, '/api/v2/message')
+api.add_resource(sendCall, '/api/v2/call')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8888)
